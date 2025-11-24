0c5582c798ba750a05feb2e45bc23ad540f97984
oscaredu1226 <oscaredu01@hotmail.com>
2025-11-24 13:37:16 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/tukuntechmssupport/TukuntechMsSupportApplication.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechmssupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TukuntechMsSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TukuntechMsSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/tukuntechmssupport/TukuntechMsSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.tukuntechmssupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TukuntechMsSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
